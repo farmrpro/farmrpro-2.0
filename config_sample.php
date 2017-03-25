fc1a413fb437e7c2a6f7c24e8458c3a143770a81
Sebastian Schwaner <herr.schwaner@gmail.com>
2017-03-25 12:46:58 +0100

    Setting up basic structure

---
+++ config_sample.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * Copyright (C) 2017 Sebastian Schwaner
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+# enter the mysql server address or IP
+define("env_mysql_server","");
+
+# enter a valid user
+define("env_mysql_user","");
+  
+# enter a password for the given user
+define("env_mysql_pass","");
+
+# enter the database to select
+define("env_mysql_database","");
+
+# enter the prefix to use for the tables
+define("env_mysql_table_prefix","");
